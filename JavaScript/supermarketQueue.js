4d576cb50579baf404b9ec3ad1d9731edb0edbf0
Elijah Williams <say@helloelijah.com>
2019-11-21 16:24:48 -0600

    exercise completed by forfeiture

---
+++ JavaScript/supermarketQueue.js
@@ -0,0 +1,45 @@
+// Codewars: not completed forfeitted
+/* DIRECTIONS
+	There is a queue for the self-checkout tills at the supermarket.
+	Your task is write a function to calculate the total time required
+	for all the customers to check out!
+
+	*INPUT*
+	customers: an array of positive integers representing the queue.
+	Each integer represents a customer, and its value is the amount
+	of time they require to check out.
+
+	n: a positive integer, the number of checkout tills.
+
+	*OUTPUT*
+	The function should return an integer, the total time required.
+*/
+
+const STATUS = {ready: 'READY', busy: 'BUSY'};
+
+function queueTime(customers, n) {
+	let tills = Array(n).fill(0);
+
+	customers.forEach((customer) => {
+		let nextTill = tills.indexOf(Math.min(...tills))
+		tills[nextTill] += customer;
+	});
+
+	return Math.max(...tills);
+}
+
+console.log(queueTime([2, 3, 7, 1], 2));
+
+/* EXAMPLES 
+	queueTime([5,3,4], 1)
+	// should return 12
+	// because when there is 1 till, the total time is just the sum of the times
+
+	queueTime([10,2,3,3], 2)
+	// should return 10
+	// because here n=2 and the 2nd, 3rd, and 4th people in the 
+	// queue finish before the 1st person has finished.
+
+	queueTime([2,3,10], 2)
+	// should return 12
+*/
