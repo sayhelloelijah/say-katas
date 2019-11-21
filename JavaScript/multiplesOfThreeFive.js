d3080ef919813fb3f3891f67849df4c2bcf3af2c
Elijah Williams <say@helloelijah.com>
2019-11-21 15:14:15 -0600

    complete multiples of 3 and 5 kata

---
+++ JavaScript/multiplesOfThreeFive.js
@@ -0,0 +1,27 @@
+// Codewars: Completed 11/21
+/* DIRECTION:
+	If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+	The sum of these multiples is 23.√
+
+	Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+
+	Note: If the number is a multiple of both 3 and 5, only count it once.√
+*/
+
+function solution(number){
+	let nums = [];
+
+	for (let i = number - 1; i > 0; i--) {
+		if(i % 3 == 0 && i % 5 == 0) {
+			nums.push(i);
+		} else if (i % 3 == 0 || i % 5 == 0) {
+			nums.push(i);
+		}
+	}
+
+	if (!nums.length) {
+		return 0;
+	}
+
+	return nums.reduce((prev, curr) => prev + curr);
+}
