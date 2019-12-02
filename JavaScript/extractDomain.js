f4dbec63b803f8e2986eed3ffc94c75d6f3bb9c6
Elijah Williams <say@helloelijah.com>
2019-12-02 16:37:07 -0600

    :white_check_mark: complete challenge

---
+++ JavaScript/extractDomain.js
@@ -0,0 +1,20 @@
+/* DIRECTIONS:
+	Write a function that when given a URL as a string,
+	parses out just the domain name and returns it as a string.
+	
+	For example:
+	domainName("http://github.com/carbonfive/raygun") == "github" 
+	domainName("http://www.zombie-bites.com") == "zombie-bites"
+	domainName("https://www.cnet.com") == "cnet"
+*/
+
+/**
+* Domain Name Extraction Function
+* 
+* @param {String} url		URL string ex. https://github.com/sayhelloelijah
+* @returns {String}     Value of domain name without extension ex. https://github.com/sayhelloelijah -> github
+*/
+function domainName(url){
+	url = url.replace('https://', '').replace('http://', '').replace('www.', '').split('.');
+	return url[0];
+}
